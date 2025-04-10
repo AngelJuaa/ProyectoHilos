3fd2d97121f3c3c2753ebe04cbc47f9eecd4e1aa
angel <angel@172.16.22.215>
2025-04-10 09:51:00 -0600

    Commit de la clase del Juves 10

--- src/main/java/Executor/EjemploExecutor.java
+++ src/main/java/Executor/EjemploExecutor.java
@@ -5,19 +5,38 @@
 package Executor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author angel
  */
 public class EjemploExecutor {
-    public static void main(String[] args) {
-        Executor ex = new Executor(){
-            @Override
-            public void execute(Runnable command) {
-                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public static void main(String[] args) throws InterruptedException{
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        
+        Runnable tarea = ()->{
+        System.out.println("inicio de la tarea");
+            try {
+                System.out.println("nombre del Thread" + Thread.currentThread().getName());
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                System.out.println(ex);
             }
-            
         };
+        //ejectar la tarea
+        executor.submit(tarea);
+        //cerrar la entrada de tareas
+        executor.shutdown();
+        
+        System.out.println("Metodo main se esta ejecutando 1" );
+        
+        executor.awaitTermination(2, TimeUnit.SECONDS);
+        System.out.println("Metodo main se sigue ejecuntando 1");
     }
 }
